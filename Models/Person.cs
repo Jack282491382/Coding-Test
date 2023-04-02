5d5a0ace4b871b897d79cded87be4622966bd3a3
Jack282491382 <129629518+Jack282491382@users.noreply.github.com>
2023-04-02 21:43:13 +1300

    Add files via upload

---
+++ Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage="Please enter your first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        public string LastName { get; set; }
+    }
+}
